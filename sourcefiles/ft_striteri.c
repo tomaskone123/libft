81d1f682f5803f25d67f5120973fecc016329748
tomaskone123 <tkkrumlovska@seznam.cz>
2024-06-04 16:53:05 +0200

    fixed striteri now waiting for internet

--- sourcefiles/ft_striteri.c
+++ sourcefiles/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: tomas <tomas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 16:44:11 by tomas             #+#    #+#             */
-/*   Updated: 2024/05/01 21:14:27 by tomas            ###   ########.fr       */
+/*   Updated: 2024/06/04 16:46:35 by tomas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,33 @@
 #include <stdio.h>
 #include <string.h>
 
+// void
+// iter(unsigned int i, char * s) {
+// 	*s += i;
+// }
+
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	int	i;
 
 	i = 0;
-	while (s[i] != '\0')
+	if (!s)
+		return;
+	while (s[i])
 	{
-		f(i, s);
-		++i;
+		//0  0
+		//1  1
+		//2  2
+		f(i, s + i);
+		i++;
 	}
 }
+
+// int main(void)
+// {
+// 	char s[] = "0000000000";
+// 	ft_striteri(s, iter);
+// 	// int	i;
+// 	// i = ft_strncmp(s, "0123456789", -1);
+// 	printf("%s\n", s);
+// }
